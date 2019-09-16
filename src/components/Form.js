91ac69119945ff83fc1d92b1858a93b7e1721ea6
Konstantin Travin <31346208+kotrvn@users.noreply.github.com>
2019-09-16 23:17:21 +0300

    push app

---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import React, {useState, useContext} from 'react';
+import { AlertContext } from '../context/alert/alertContext';
+
+export const Form = () => {
+
+    const [value, setValue] = useState('')
+    const alert = useContext(AlertContext)
+
+    const submitHandler = event => {
+        event.preventDefault()
+
+        alert.show(value, 'success');
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='form-group'>
+                <input 
+                    className='form-control' 
+                    type='text'
+                    placeholder='Введите название заметки'
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    />
+
+            </div>
+        </form>
+    )
+}
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => (
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-4">Лучшее реакт приложение</h1>
+            <p class="lead">Версия приложения <strong>1.0.42</strong></p>
+        </div>
+    </div>
+)
