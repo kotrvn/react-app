0a30cd6800b9db5fe84cb7c623865369056d83b5
Konstantin Travin <31346208+kotrvn@users.noreply.github.com>
2019-09-17 17:23:16 +0300

    Add functions, deploy

---
+++ build/precache-manifest.777548f8d6e3da82a96696ae47969c2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401710d2e792712f22b1b2dd94eefe03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f184d260ee58de921a22",
+    "url": "/static/css/main.2b916b98.chunk.css"
+  },
+  {
+    "revision": "ef000902105c7bdf08e4",
+    "url": "/static/js/2.46291d71.chunk.js"
+  },
+  {
+    "revision": "f184d260ee58de921a22",
+    "url": "/static/js/main.59027c28.chunk.js"
+  },
+  {
+    "revision": "069f568d796bf004e8c7",
+    "url": "/static/js/runtime~main.4684f7a5.js"
+  }
+]);
---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Loader = () => (
+     // eslint-disable-next-line
+    <div className='text-center'>
+        <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+        </div>
+    </div>
+)
+ 
